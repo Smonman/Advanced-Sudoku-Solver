da8bb041e9de72074647ce1315a112c7e5e6c0e6
Simon Kreuzpointner <smonman7@gmail.com>
2018-11-21 18:02:11 +0100

    Add project files.

---
+++ AdvancedSudokuSolver/Grid.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace AdvancedSudokuSolver
+{
+	class Grid
+	{
+		// size of the grid currently constantly 9
+		public int size;
+
+		// actual grid
+		public Tile[,] grid;
+
+		public Grid()
+		{
+			size = 9;
+			grid = new Tile[size, size];
+
+			// loop through the grid grid
+			for(int y = 0; y < size; y++)
+			{
+				for(int x = 0; x < size; x++)
+				{
+					// generate a new tile for each spot with 0 as a number
+					grid[x, y] = new Tile(x, y, false, 0);
+				}
+			}
+		}
+
+		// fills the grid grid
+		public void Fill(int[,] input)
+		{
+			// if the sizes do not match throw
+			if(input.GetLength(0) != size || input.GetLength(1) != size)
+			{
+				throw new Exception("Grid Fill: The Size does not match");
+			}
+			else
+			{
+				// else loop throug the input matrix
+				for(int y = 0; y < size; y++)
+				{
+					for(int x = 0; x < size; x++)
+					{
+						// get the number from the input matrix
+						int number = input[y, x];
+
+						// if the number is zero
+						if(number == 0)
+						{
+							// set void at the spot
+							grid[x, y].SetVoid();
+						}
+						else
+						{
+							// else set the corresponding number
+							grid[x, y].SetNumber(number);
+						}
+					}
+				}
+			}
+		}
+
+		// removes the number from all of a tiles peers
+		public bool RemoveFromPeers(int xCoord, int yCoord, int number)
+		{
+			// if this is true somwhere in here an update occured
+			// and return true so the whole proccess is called later again
+			bool changedMaster = false;
+
+			// Row
+			// loop through the row of the tile
+			for(int x = 0; x < size; x++)
+			{
+				// if the current position is the same as the input tile continue
+				if(x == xCoord)
+				{
+					continue;
+				}
+
+				// else remove the number from the possible numbers list
+				bool changedRow = grid[x, yCoord].RemoveFromPossible(number);
+				// if something changed update changed master
+				if(changedRow)
+				{
+					changedMaster = true;
+				}
+			}
+
+			// Col
+			// same as row but only collum
+			for(int y = 0; y < size; y++)
+			{
+				if(y == yCoord)
+				{
+					continue;
+				}
+
+				bool changedCol = grid[xCoord, y].RemoveFromPossible(number);
+				if(changedCol)
+				{
+					changedMaster = true;
+				}
+			}
+
+			// Block
+			// gets the index of the corresponding block where the tile is inside
+			// blockX ranges from 0 to 2 from left to right
+			int blockX = (int)Math.Floor((double)xCoord / (double)3);
+			// blockY ranges from 0 to 2 from top to bottom
+			int blockY = (int)Math.Floor((double)yCoord / (double)3);
+
+			// loop through a 3 x 3 chunk
+			for(int i = 0; i < 3; i++)
+			{
+				// adjust the indecies to correspond to the correct block
+				int y = i + blockY * 3;
+
+				for(int j = 0; j < 3; j++)
+				{
+					int x = j + blockX * 3;
+
+					// if the currently looked at spot is the same as the input tile skip
+					if(y == yCoord && x == xCoord)
+					{
+						continue;
+					}
+
+					// else remove the number from the possible numbers list
+					bool changedBlock = grid[x, y].RemoveFromPossible(number);
+					// if something changed update changed master
+					if(changedBlock)
+					{
+						changedMaster = true;
+					}
+				}
+			}
+
+			// return the changedMaster to inform the caller that something did update
+			return changedMaster;
+		}
+	}
+}
---
+++ AdvancedSudokuSolver/Tile.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+
+namespace AdvancedSudokuSolver
+{
+	class Tile
+	{
+		// x coord inside the matrix
+		public int x;
+
+		// y coord inside the matrix
+		public int y;
+
+		// is true if it has a final number
+		public bool hasNumber;
+
+		// if has number is true this is the number
+		public int number;
+
+		// list of all possible numbers
+		public List<int> possibleNumbers;
+
+		// returns the amount of possible numbers
+		public int possibleNumbersAmount
+		{
+			get
+			{
+				return possibleNumbers.Count;
+			}
+		}
+
+		public Tile(int x, int y, bool hasNumber, int number)
+		{
+			this.x = x;
+			this.y = y;
+			this.hasNumber = hasNumber;
+			if(hasNumber)
+			{
+				possibleNumbers.Clear();
+				this.number = number;
+			}
+			else
+			{
+				// if it does not have a number from the start define all possible numbers
+				possibleNumbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			}
+		}
+
+		// Sets a specific number
+		public void SetNumber(int number)
+		{
+			this.number = number;
+			hasNumber = true;
+			// clears the possible numbers because it has
+			// a number now
+			possibleNumbers.Clear();
+		}
+
+		// Sets a number from the last possible number
+		public void SetNumberFromLastPossibleNumber()
+		{
+			SetNumber(possibleNumbers[0]);
+		}
+
+		// sets no number
+		public void SetVoid()
+		{
+			// sets the number to zero
+			this.number = 0;
+			hasNumber = false;
+			// sets every possible number as possible numbers
+			possibleNumbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		}
+
+		// removes a specific number from the possible numbers
+		// and returns true if something changed
+		public bool RemoveFromPossible(int number)
+		{
+			// if the list contains the number
+			if(possibleNumbers.Contains(number))
+			{
+				// remove it
+				// and return true because something
+				// indeed did change
+				possibleNumbers.Remove(number);
+				return true;
+			}
+			else
+			{
+				// else no need to remove something
+				// that is not there
+				// and nothing changed
+				return false;
+			}
+		}
+	}
+}
